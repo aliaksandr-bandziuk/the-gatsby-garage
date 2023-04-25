0436f7815581c66720453a1ad2bf96ec18e921f2
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-04-25 20:38:11 +0200

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import * as React from "react";
 
 const IndexPage = () => {
-  return <main />;
+  return (
+    <main>
+      <h1>this is the homepage</h1>
+    </main>
+  );
 };
 
 export default IndexPage;
---
+++ src/pages/test.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>this is the test page</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => (
+  <>
+    <meta name="description" content="this is the page description"></meta>
+    <title>This is the test page</title>
+  </>
+);
